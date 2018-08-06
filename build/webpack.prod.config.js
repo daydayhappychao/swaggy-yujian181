0b43f399bab15890496db8667592c5ba488bb937
magin <wangchaomagin@gmail.com>
2018-08-06 14:27:53 +0800

    css压缩

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -1,11 +1,22 @@
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const optimizeCss = require('optimize-css-assets-webpack-plugin');
 const base = require('./webpack.base.config.js');
 
 module.exports = merge(base, {
   mode: 'development',
   devtool: 'source-map',
   plugins: [
-    // new UglifyJSPlugin()
-  ]
+    new UglifyJSPlugin(),
+    new optimizeCss({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorOptions: { discardComments: { removeAll: true } },
+      canPrint: true
+    }),
+  ],
+  optimization: {
+    // minimize: true,
+    minimizer: [new optimizeCss({})],
+  }  
 });
